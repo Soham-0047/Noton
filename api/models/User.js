d81d2ad58b03a07d56a75e5ce2a20a8066b050d1
Soham <solutionnoton@gmail.com>
2022-04-03 12:02:12 +0530

    Added client and api

--- api/models/User.js
+++ api/models/User.js
@@ -5,5 +5,17 @@ A Mongoose schema defines the structure of the document, default values, validat
 
 */
 const UserSchema = new mongoose.Schema({
+    username:{type:String,required:true,unique:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,require:true},
+    profilePic:{type:String,default:""},
+    isAdmin:{type:Boolean,default:false}
+},
+{timestamps:true}
+)
 
-})
+
+
+
+
+module.exports = mongoose.model("User",UserSchema)
---
+++ api/routes/users.js
@@ -0,0 +1,140 @@
+const router = require("express").Router();
+const User = require("../models/User")
+const CryptoJS = require("crypto-js")
+
+const verify = require("../verifyToken")
+
+//update
+
+router.put("/:id",verify,async(req,res)=>{
+    if(req.user.id === req.params.id || req.user.isAdmin){
+        if(req.body.password){
+            req.body.password = CryptoJS.AES.encrypt(req.body.password,
+            process.env.SECRET_KEY).toString()
+        }
+    
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+
+            $set:req.body,
+        },
+        
+        {new:true}
+        )
+        res.status(200).json(updatedUser)
+        
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
+else{
+    res.status(403).json("only update your account")
+ }
+
+})
+
+
+//delete
+
+router.delete("/:id",verify,async(req,res)=>{
+    if(req.user.id === req.params.id || req.user.isAdmin){
+        
+    
+    try {
+        await User.findByIdAndDelete(req.params.id)
+
+        res.status(200).json("User successfully deleted")
+        
+    } 
+    catch (err) {
+        res.status(500).json("error")
+    }
+}
+else{
+    res.status(403).json("you can delete your account")
+ }
+
+})
+
+//get
+router.get("/find/:id",async(req,res)=>{
+    
+        
+    
+    try {
+        const user = await User.findById(req.params.id) 
+
+        const {password,...info} = user._doc;
+
+        res.status(200).json(info)
+        
+    } 
+    catch (err) {
+        res.status(500).json(err)
+    }
+
+
+
+})
+
+
+//get all users
+
+router.get("/",verify,async(req,res)=>{
+    const query = req.query.new;
+
+    if(req.user.isAdmin){
+        
+    
+    try {
+        const users = query ? await User.find().sort({_id:-1}).limit(5) : await User.find()
+
+        res.status(200).json(users)
+        
+    } 
+    catch (err) {
+        res.status(500).json("error")
+    }
+}
+else{
+    res.status(403).json("you are not allowed to see all the users")
+ }
+
+})
+
+
+
+//Get user stats
+
+router.get("/stats",async (req,res)=>{
+    const today = new Date()
+    const lastYear = today.setFullYear(today.setFullYear() -1)
+
+
+    try {
+        const data = await User.aggregate([
+            {
+                $project:{
+                    month:{$month:"$createdAt"},
+                },
+            },
+            {
+                $group:{
+                    _id:"$month",
+                    total:{$num:1},
+                },
+            },
+
+        ])
+
+        res.status(200).json(data)
+        
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+
+module.exports = router
